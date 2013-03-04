20f0a63b0291856e6e4f84a388fcee2e6520d5c2
d1rk <d1rk@gmx.de>
2013-03-04 10:11:03 +0100

    pull view files from all possible locations

--- controllers/ActivitiesController.php
+++ controllers/ActivitiesController.php
@@ -7,6 +7,32 @@ use li3_activities\core\Activity;
 
 class ActivitiesController extends \lithium\action\Controller {
 
+	public function _init() {
+		parent::_init();
+		$this->_render['paths'] = array(
+			'template' => array(
+				LITHIUM_APP_PATH . '/views/{:controller}/{:template}.{:type}.php',
+				LI3_ACTIVITIES_PATH . '/views/{:controller}/{:template}.{:type}.php',
+				'{:library}/views/{:controller}/{:template}.{:type}.php',
+			),
+			'layout' => array(
+				LITHIUM_APP_PATH . '/views/layouts/{:layout}.{:type}.php',
+				LI3_ACTIVITIES_PATH . '/views/layouts/{:layout}.{:type}.php',
+				'{:library}/views/layouts/{:layout}.{:type}.php',
+			),
+			'element' => array(
+				LITHIUM_APP_PATH . '/views/elements/{:template}.{:type}.php',
+				LI3_ACTIVITIES_PATH . '/views/elements/{:template}.{:type}.php',
+				'{:library}/views/elements/{:template}.{:type}.php',
+			),
+			'mustache' => array(
+				LITHIUM_APP_PATH . '/views/mustache/{:template}.{:type}.php',
+				LI3_ACTIVITIES_PATH . '/views/mustache/{:template}.{:type}.php',
+				'{:library}/views/mustache/{:template}.{:type}.php',
+			),
+		);
+	}
+
 	public function index($input = null) {
 		$defaults = array('limit' => 250, 'order' => array('created' => 'DESC'));
 		if (!empty($input) && is_array($input)) {
